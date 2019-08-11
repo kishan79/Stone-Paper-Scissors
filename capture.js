e96bb27aa920a322bfad8a2ca3cf83853c660d9c
Kishan <kishan.237@gmail.com>
2019-08-11 13:55:41 +0530

    animation added

--- capture.js
+++ capture.js
@@ -5,103 +5,101 @@ const streaming = false;
 let imageUrl = new Map();
 let obj = {};
 
-let captureBtns = document.querySelectorAll('.capture');
+let captureBtns = document.querySelectorAll(".capture");
 captureBtns.forEach((d, index) => {
-    d.addEventListener('click', () => {
-        handlecamera(d, `cameraid${index}`);
-    });
+  d.addEventListener("click", () => {
+    handlecamera(d, `cameraid${index}`);
+  });
 });
 
-
-const generateCameraViewPort = (id) => {
-    const cameraVideo = document.createElement('video');
-    cameraVideo.setAttribute('id', id);
-    cameraVideo.setAttribute('height', '60%');
-    cameraVideo.setAttribute('width', '60%');
-    cameraVideo.style.borderRadius = '50%';
-    return cameraVideo;
-}
+const generateCameraViewPort = id => {
+  const cameraVideo = document.createElement("video");
+  cameraVideo.setAttribute("id", id);
+  cameraVideo.setAttribute("height", "60%");
+  cameraVideo.setAttribute("width", "60%");
+  cameraVideo.style.borderRadius = "50%";
+  return cameraVideo;
+};
 
 const handlecamera = (d, id) => {
-    let cameraContainer = d.parentElement.querySelector('.camera');
-    if (!!cameraContainer) {
-        if (!cameraContainer.contains(document.querySelector('video'))) {
-            cameraContainer.innerHTML = null;
-            const cameraViewPort = generateCameraViewPort(id);
-            cameraContainer.append(cameraViewPort);
-            openCamera(d, cameraViewPort, cameraContainer, id);
-        } else {
-            console.log('Already an instance is running with the existing ID');
-        }
+  let cameraContainer = d.parentElement.querySelector(".camera");
+  if (!!cameraContainer) {
+    if (!cameraContainer.contains(document.querySelector("video"))) {
+      cameraContainer.innerHTML = null;
+      const cameraViewPort = generateCameraViewPort(id);
+      cameraContainer.append(cameraViewPort);
+      openCamera(d, cameraViewPort, cameraContainer, id);
+    } else {
+      console.log("Already an instance is running with the existing ID");
     }
-}
-
+  }
+};
 
 const openCamera = (captureBtn, video, cameraContainer, id) => {
-    captureBtn.innerHTML = 'Capture Image';
-    navigator.mediaDevices
-        .getUserMedia({ video: true, audio: false })
-        .then((stream) => {
-            video.srcObject = stream;
-            video.play();
-            captureBtn.addEventListener('click', () => {
-                captureImage(captureBtn, video, cameraContainer, id, stream);
-                stopStream(stream);
-            });
-        })
-        .catch(function (err) {
-            console.log('An error occurred: ' + err);
-        });
-
-}
-
-const stopStream = (stream) => {
-    stream.getVideoTracks().forEach((track) => {
-        track.stop();
+  captureBtn.innerHTML = "Capture Image";
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then(stream => {
+      video.srcObject = stream;
+      video.play();
+      captureBtn.addEventListener("click", () => {
+        captureImage(captureBtn, video, cameraContainer, id, stream);
+        stopStream(stream);
+      });
+    })
+    .catch(function(err) {
+      console.log("An error occurred: " + err);
     });
-}
+};
+
+const stopStream = stream => {
+  stream.getVideoTracks().forEach(track => {
+    track.stop();
+  });
+};
 
 const captureImage = (captureBtn, video, cameraContainer, id) => {
-    let imageCanvas = document.createElement('canvas');
-    imageCanvas.height = '150';
-    imageCanvas.width = '180';
-    imageCanvas.style.borderRadius = '50%';
-    imageCanvas.setAttribute('id', 'picCanvas');
-    cameraContainer.append(imageCanvas);
-    let ctx = imageCanvas.getContext('2d');
-    ctx.drawImage(video, 0, 0, 200, 200);
-    let data = imageCanvas.toDataURL('image/png');
+  let imageCanvas = document.createElement("canvas");
+  imageCanvas.height = "150";
+  imageCanvas.width = "180";
+  imageCanvas.style.borderRadius = "50%";
+  imageCanvas.setAttribute("id", "picCanvas");
+  cameraContainer.append(imageCanvas);
+  let ctx = imageCanvas.getContext("2d");
+  ctx.drawImage(video, 0, 0, 200, 200);
+  let data = imageCanvas.toDataURL("image/png");
 
-    imageUrl.set(`${id}`, data);
+  imageUrl.set(`${id}`, data);
 
-    // console.log(imageUrl);
-    obj = Array.from(imageUrl).reduce((obj, [key, value]) => (
-        Object.assign(obj, { [key]: value })
-    ), {});
+  // console.log(imageUrl);
+  obj = Array.from(imageUrl).reduce(
+    (obj, [key, value]) => Object.assign(obj, { [key]: value }),
+    {}
+  );
 
-    captureBtn.innerHTML = "Recapture";
-    captureBtn.removeEventListener('click', handlecamera);
-    captureBtn.addEventListener('click', () => {
-        location.reload();
-    });
-    cameraContainer.querySelector('video').style.display = 'none';
-}
+  captureBtn.innerHTML = "Recapture";
+  captureBtn.removeEventListener("click", handlecamera);
+  captureBtn.addEventListener("click", () => {
+    location.reload();
+  });
+  cameraContainer.querySelector("video").style.display = "none";
+};
 
-document.getElementById('decide').addEventListener('click', () => {
-    saveprofilepics(obj);
+document.getElementById("decide").addEventListener("click", () => {
+  saveprofilepics(obj);
 });
 
 function saveprofilepics(obj) {
-    if (localStorage.getItem('details') === null) {
-        let userDetails = [];
-        userDetails.push(obj);
-        localStorage.setItem('details', JSON.stringify(userDetails));
-    } else {
-        let userDetails = JSON.parse(localStorage.getItem('details'));
-        userDetails.push(obj);
-        localStorage.setItem('details', JSON.stringify(userDetails));
-    }
-    document.getElementById('profilepic1').src = obj.cameraid0;
-    document.getElementById('profilepic2').src = obj.cameraid1;
-    document.getElementById('profilepic3').src = obj.cameraid2;
+  if (localStorage.getItem("details") === null) {
+    let userDetails = [];
+    userDetails.push(obj);
+    localStorage.setItem("details", JSON.stringify(userDetails));
+  } else {
+    let userDetails = JSON.parse(localStorage.getItem("details"));
+    userDetails.push(obj);
+    localStorage.setItem("details", JSON.stringify(userDetails));
+  }
+  document.getElementById("profilepic1").src = obj.cameraid0;
+  document.getElementById("profilepic2").src = obj.cameraid1;
+  document.getElementById("profilepic3").src = obj.cameraid2;
 }
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-document.getElementById('myForm').addEventListener('submit', saveDetails);
-let gameAreaBtn = document.getElementById('gameArea');
+document.getElementById("myForm").addEventListener("submit", saveDetails);
+let gameAreaBtn = document.getElementById("gameArea");
 
 gameAreaBtn.style.display = "none";
 
@@ -8,144 +8,162 @@ gameAreaBtn.style.display = "none";
 // })
 
 function saveDetails(e) {
-    let player1 = document.getElementById('player1name').value;
-    let playerOneChoice = document.getElementById('player1choice').value;
+  let player1 = document.getElementById("player1name").value;
+  let playerOneChoice = document.getElementById("player1choice").value;
 
-    let player2 = document.getElementById('player2name').value;
-    let playerTwoChoice = document.getElementById('player2choice').value;
+  let player2 = document.getElementById("player2name").value;
+  let playerTwoChoice = document.getElementById("player2choice").value;
 
-    let player3 = document.getElementById('player3name').value;
-    let playerThreeChoice = document.getElementById('player3choice').value;
+  let player3 = document.getElementById("player3name").value;
+  let playerThreeChoice = document.getElementById("player3choice").value;
 
-    let stat = document.getElementById('stat').value;
+  let stat = document.getElementById("stat").value;
 
-    let userData = {
-        player1,
-        playerOneChoice,
-        player2,
-        playerTwoChoice,
-        player3,
-        playerThreeChoice,
-        stat
-    }
+  let userData = {
+    player1,
+    playerOneChoice,
+    player2,
+    playerTwoChoice,
+    player3,
+    playerThreeChoice,
+    stat
+  };
 
-    if (localStorage.getItem('details') === null) {
-        let userDetails = [];
-        userDetails.push(userData);
-        localStorage.setItem('details', JSON.stringify(userDetails));
+  if (localStorage.getItem("details") === null) {
+    let userDetails = [];
+    userDetails.push(userData);
+    localStorage.setItem("details", JSON.stringify(userDetails));
+  } else {
+    let userDetails = JSON.parse(localStorage.getItem("details"));
+    let currUser = userDetails[userDetails.length - 1];
+    currUser = Object.assign(currUser, userData);
+    userDetails[userDetails - 1] = currUser;
+    // userDetails.push(userData);
+    localStorage.setItem("details", JSON.stringify(userDetails));
+  }
 
-    } else {
+  document.getElementById("GamePad").style.display = "none";
+  gameAreaBtn.style.display = "block";
 
-        let userDetails = JSON.parse(localStorage.getItem('details'));
-        let currUser = userDetails[userDetails.length -1];
-        currUser = Object.assign(currUser,userData);
-        userDetails[userDetails - 1] = currUser;
-        // userDetails.push(userData);
-        localStorage.setItem('details', JSON.stringify(userDetails));
+  document.getElementById("statement").innerHTML = userData.stat;
 
-    }
+  document.getElementById("player1naam").innerHTML = userData.player1;
+  document.getElementById("player2naam").innerHTML = userData.player2;
+  document.getElementById("player3naam").innerHTML = userData.player3;
 
-    document.getElementById('GamePad').style.display = "none";
-    gameAreaBtn.style.display = "block";
-    // gameOn();
-
-    e.preventDefault();
+  document.getElementById("player01choice").innerHTML =
+    userData.playerOneChoice;
+  document.getElementById("player02choice").innerHTML =
+    userData.playerTwoChoice;
+  document.getElementById("player03choice").innerHTML =
+    userData.playerThreeChoice;
+  e.preventDefault();
 }
 
 function generateResult() {
-    let min = 1, max = 4;
-    let random = Math.floor(Math.random() * (+max - +min)) + +min;
-    return random;
+  let min = 1,
+    max = 4;
+  let random = Math.floor(Math.random() * (+max - +min)) + +min;
+  return random;
 }
 
 function assignLabel(x) {
-    if (x == 1) {
-        return "Stone";
-    } else if (x == 2) {
-        return "Paper";
-    } else if (x == 3){
-        return "Scissors";
-    } else if(x == 0) {
-        return "Tie";
-    }
+  if (x == 1) {
+    return "Stone";
+  } else if (x == 2) {
+    return "Paper";
+  } else if (x == 3) {
+    return "Scissors";
+  } else if (x == 0) {
+    return "Tie";
+  }
 }
-
 
 function gameOn() {
+  console.log("hello");
+  clearInterval(t);
+  console.log(t);
+  console.log("hello1");
+  let playerDetails = JSON.parse(localStorage.getItem("details"));
+  let currMatch = playerDetails[playerDetails.length - 1];
+  // console.log(currMatch);
 
-    let playerDetails = JSON.parse(localStorage.getItem('details'));
-    let currMatch = playerDetails[playerDetails.length - 1];
-    console.log(currMatch);
+  let playerScores = {
+    player1score: generateResult(),
+    player2score: generateResult(),
+    player3score: generateResult()
+  };
+  currMatch = Object.assign(currMatch, playerScores);
 
-    let playerScores = {
-        player1score: generateResult(),
-        player2score: generateResult(),
-        player3score: generateResult()
-    }
-    currMatch = Object.assign(currMatch, playerScores);
-    
-    console.log(playerScores);
+  // console.log(playerScores);
 
-    playerDetails[playerDetails.length - 1] = currMatch;
-    localStorage.setItem('details', JSON.stringify(playerDetails));
+  playerDetails[playerDetails.length - 1] = currMatch;
+  localStorage.setItem("details", JSON.stringify(playerDetails));
 
-    document.getElementById('player1').innerHTML = assignLabel(currMatch.player1score);
-    document.getElementById('player2').innerHTML = assignLabel(currMatch.player2score);
-    document.getElementById('player3').innerHTML = assignLabel(currMatch.player3score);
+  document.getElementById("player1").innerHTML = assignLabel(
+    currMatch.player1score
+  );
+  document.getElementById("player2").innerHTML = assignLabel(
+    currMatch.player2score
+  );
+  document.getElementById("player3").innerHTML = assignLabel(
+    currMatch.player3score
+  );
+  let set = new Set([
+    playerScores.player1score,
+    playerScores.player2score,
+    playerScores.player3score
+  ]);
+  // console.log(set);
 
-    let set = new Set([playerScores.player1score, playerScores.player2score, playerScores.player3score]);
-    console.log(set);
+  let label = evaluator(set);
 
-    let label = evaluator(set);   
-        
-    let arr = Object.values(playerScores);
+  let arr = Object.values(playerScores);
 
-    let win =  winner(label,arr,currMatch);
-    console.log(win);
+  let win = winner(label, arr, currMatch);
+  // console.log(win);
 
-    let z = win.toString();
-    document.getElementById('result').innerHTML = `<h3>${z}</h3>`;
+  let z = win.toString();
+  document.getElementById("result").innerHTML = `<h3>${z}</h3>`;
 
-
-    
+  document.getElementById('playButton').dis
 }
 
-function winner(x,y,z) {
-
-    // let player = ["Player 1","Player 2","Player 3"];
-    let player = [z.player1,z.player2,z.player3];
-    let w = [];
-    if(x == 0)
-        return "Match Tie";
-    else if(x == 4){
-        // console.log(player[x]);
-        return "Match Tie";
+function winner(x, y, z) {
+  // let player = ["Player 1","Player 2","Player 3"];
+  let player = [z.player1, z.player2, z.player3];
+  let w = [];
+  if (x == 0) return "Match Tie";
+  else if (x == 4) {
+    return "Match Tie";
+  } else {
+    for (let i = 0; i < y.length; i++) {
+      if (x == y[i]) w.push(player[i]);
     }
-    else {
-    for(let i=0;i<y.length;i++){
-        if(x==y[i])
-            w.push(player[i]);
-        }
     return w;
-    }
+  }
 }
 
-
-function evaluator(y){
-    
-    if(y.size == 1){
-        return 4;
-    }
-    else if(y.size == 3){
-        return 0;
-    }
-    else if(y.size == 2){
-        if(y.has(1) && y.has(2))
-            return 2;
-        else if(y.has(2) && y.has(3))
-            return 3;
-        else if(y.has(1) && y.has(3))
-            return 1;
-    }
+function evaluator(y) {
+  if (y.size == 1) {
+    return 4;
+  } else if (y.size == 3) {
+    return 0;
+  } else if (y.size == 2) {
+    if (y.has(1) && y.has(2)) return 2;
+    else if (y.has(2) && y.has(3)) return 3;
+    else if (y.has(1) && y.has(3)) return 1;
+  }
 }
 
+let t = setInterval(() => {
+  document.querySelectorAll(".randomImage").forEach(index => {
+    index.src = getRandomString();
+    index.classList.add("heartBeat", "infinite", "animated", "faster");
+  });
+}, 500);
+
+function getRandomString() {
+  const imageUrl = ["/img/stone.png", "/img/paper.png", "/img/scissors.png"];
+  return imageUrl[Math.floor(Math.random() * Math.floor(3))];
+}
